cb915b809c8c14b930215db880ea7533cc05a1b3
zukizukizuki <98022551+zukizukizuki@users.noreply.github.com>
2022-03-13 11:58:06 +0900

    tor

--- tor.py
+++ tor.py
@@ -1,25 +1,13 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 import subprocess
 
-from selenium.webdriver.chrome.options import Options
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
+cmd = f'"C:\\Users\\asaka\\OneDrive\\Desktop\\Tor Browser\\Browser\\firefox.exe"'
 
-def Tor_start():
-    subprocess.call(r'taskkill /F /T /IM firefox.exe', stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-    Tor = f'"C:\\Users\\asaka\\OneDrive\\Desktop\\Tor Browser\\Browser\\firefox.exe"'
-    subprocess.Popen(Tor)
+p = subprocess.Popen(cmd)
+options = Options()
+options.add_argument('--proxy-server=socks5://127.0.0.1:9150')
 
-def Selenium_start():
-    options = Options()
-    #options.add_argument('--headless')
-    options.add_argument('--proxy-server=socks5://127.0.0.1:9050')
-    driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
-    driver.implicitly_wait(20)
-    driver.get(url)
-    return driver
-
-url = ''
-
-if __name__ == '__main__':
-    Tor_start()
-    driver = Selenium_start()
+driver = webdriver.Chrome(chrome_options=options)
+driver.implicitly_wait(30)
+driver.get('')
